f183efb7717303086fc5b14b46488078807e2b67
miromero13 <ilseromero35@gmail.com>
2025-06-28 10:16:48 -0400

    feat: add gender and user_type fields to User model

--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,8 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, String, Enum as SQLAlchemyEnum
 from sqlalchemy.ext.declarative import declarative_base
 import uuid
 from sqlalchemy.dialects.postgresql import UUID
+from app.schemas.enums import UserTypeEnum, GenderEnum
 
 Base = declarative_base()
 
@@ -11,4 +12,6 @@ class User(Base):
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
     name = Column(String, nullable=False)
     email = Column(String, unique=True, index=True, nullable=False)
-    hashed_password = Column(String, nullable=False)  #  Nuevo campo
+    hashed_password = Column(String, nullable=False)
+    gender = Column(SQLAlchemyEnum(GenderEnum), nullable=True)
+    user_type = Column(SQLAlchemyEnum(UserTypeEnum), nullable=True)
